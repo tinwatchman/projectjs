df3ded52b26c2db146cdb5ecaed2bb53766678f9
tinwatchman <j.stout@jonstout.net>
2015-04-19 15:27:20 -0400

    add dependency copying

--- project.js
+++ project.js
@@ -1,4 +1,5 @@
 
+/*
 var ProjectRunner = require('./lib/runner');
 
 var runner = new ProjectRunner();
@@ -6,4 +7,16 @@ var runner = new ProjectRunner();
 var path = process.cwd() + "/example/";
 
 runner.loadAndRun(path);
+*/
 
+var ProjectJsCompiler = require('./lib/compiler');
+var ProjectJsParser = require('./lib/parser');
+var compiler = new ProjectJsCompiler();
+var parser = new ProjectJsParser();
+
+var projectRoot = process.cwd() + "/example",
+    projectFile = projectRoot + "/project.json",
+    buildDir = "./build";
+
+var registry = parser.loadRegistry(projectFile);
+compiler.buildProject(registry, projectRoot, buildDir);
--- spec/CompilerSpec.js
+++ spec/CompilerSpec.js
@@ -1,9 +1,10 @@
 
 describe("ProjectJsCompiler", function() {
     var ProjectJsCompiler = require("../lib/compiler");
+    var ProjectJsRegistry = require("../lib/registry");
+    var compiler;
 
     describe("ProjectJsCompiler.addClassBoilerplate", function() {
-        var compiler;
         var use;
 
         beforeEach(function() {
@@ -48,8 +49,6 @@ describe("ProjectJsCompiler", function() {
     });
 
     describe("ProjectJsCompiler.findUses", function() {
-        var compiler;
-
         beforeEach(function() {
             compiler = new ProjectJsCompiler();
         });
@@ -95,6 +94,55 @@ describe("ProjectJsCompiler", function() {
             expect(list[0].name).toEqual('some.name_$pace.cl@ss');
             expect(list[1].name).toEqual('s0me.#Namespace.c!assTwo');
             expect(list[2].name).toEqual('%.*');
+        });
+    });
+
+    describe("ProjectJsCompiler.replaceUses", function() {
+        var registry;
+
+        beforeEach(function() {
+            compiler = new ProjectJsCompiler();
+            registry = new ProjectJsRegistry({
+                "base": "some.namespace",
+                "map": {
+                    "some.namespace.Class": "./Class",
+                    "some.namespace.OtherClass": "./OtherClass",
+                    "some.namespace.*": [
+                        "some.namespace.Class",
+                        "some.namespace.OtherClass"
+                    ]
+                },
+                "schema": {
+                    "name": "projectjs",
+                    "version": "0.0.1"
+                }
+            });
+        });
+
+        it("should exist", function() {
+            expect(compiler.replaceUses).toBeDefined();
+        });
+
+        it("should replace a use invocation within a code block with a standard require", function() {
+            var code = "var myClass = use('some.namespace.Class');";
+            expect(compiler.replaceUses(code, registry)).toEqual("var myClass = require('./Class');");
+        });
+
+        it("should replace a use package request with a package map", function() {
+            var code = "var myPackage = use('some.namespace.*');";
+            var result = compiler.replaceUses(code, registry);
+            expect(result).toEqual("var myPackage = { 'Class': require('./Class'), 'OtherClass': require('./OtherClass') };");
+        });
+    });
+
+    describe("ProjectJsCompiler.getBuildFilePath", function() {
+        beforeEach(function() {
+            compiler = new ProjectJsCompiler();
+        });
+
+        it("should produce a valid file name", function() {
+            var path = compiler.getBuildFilePath("./src/package/Class", "./build");
+            expect(path).toEqual("build/src/package/Class.js");
         });
     });
     
